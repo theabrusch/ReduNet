524e3d7d792b869de6660b4a6c1b0f4b9ad19ada
theabrusch <62242837+theabrusch@users.noreply.github.com>
2023-07-28 13:12:07 +0200

    change naming if binary

--- train_forward.py
+++ train_forward.py
@@ -31,7 +31,7 @@ device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cp
 ## Model Directory
 model_dir = os.path.join(args.save_dir, 
                          'forward',
-                         f'{args.data}+ch{args.channels}+l{args.layers}',
+                         f'{args.data}+ch{args.channels}+l{args.layers}' if not args.mnist_binary else f'{args.data}+ch{args.channels}+l{args.layers}+binary',
                          f'samples{args.samples}'
                          f'{args.tail}')
 os.makedirs(model_dir, exist_ok=True)
